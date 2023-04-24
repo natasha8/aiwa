a625ca98137c59fb4602305127f06ac0e4983db1
natasha <nat.parisella@gmail.com>
2023-04-24 11:12:57 +0200

    setup and homepage

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	experimental: {
+		appDir: true,
+		serverComponentsExternalPackages: ["@tremor/react"],
+	},
+	images: {
+		domains: ["www.weatherbit.io", "https://i.ibb.co/"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
